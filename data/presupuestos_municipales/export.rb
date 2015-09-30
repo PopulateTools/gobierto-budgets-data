83a8195cad0f1a379204db0945417b683c1a7886
Fernando Blat <ferblape@gmail.com>
2015-09-30 23:33:31 +0200

    Include Cartagena

--- data/presupuestos_municipales/export.rb
+++ data/presupuestos_municipales/export.rb
@@ -1,4 +1,6 @@
-['Gijón', 'Órgiva', 'Santiago de Compostela', 'Ciempozuelos', 'Madrid'].each do |name|
+['Gijón', 'Órgiva', 'Santiago de Compostela', 'Ciempozuelos', 'Madrid', 'Cartagena'].each do |name|
   file = PresupuestosMunicipales::DataExport.new(INE::Place.find_by_name(name)).export
   puts file
 end
+
+
