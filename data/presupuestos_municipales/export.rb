1c969dbc743ac83d29a1bc981daebb3367e2b7ab
Fernando Blat <ferblape@gmail.com>
2016-01-20 16:11:45 +0100

    Remove INE data

--- data/presupuestos_municipales/export.rb
+++
@@ -1,6 +0,0 @@
-['Gijón', 'Órgiva', 'Santiago de Compostela', 'Ciempozuelos', 'Madrid', 'Cartagena'].each do |name|
-  file = PresupuestosMunicipales::DataExport.new(INE::Place.find_by_name(name)).export
-  puts file
-end
-
-
